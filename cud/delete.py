b990218a2a51cb5085cf61ae6558d00d7a4af934
Juan Peralta <67843249+ItsZekken@users.noreply.github.com>
2023-11-23 22:33:33 -0500

    Update delete.py

--- cud/delete.py
+++ cud/delete.py
@@ -1,33 +1,34 @@
 import pymysql
-
+import requests
+import json
 
 class DATABASE:
     def __init__(self):
         self.connection = pymysql.connect(
             host="localhost", user="root", password="14062003", db="gestion_usuarios"
         )
-
         self.cursor = self.connection.cursor()
-
         print("😃")
 
-    def eliminar_usuarios(self):
+    def eliminar_usuarios(self, noDocumento):
         query = f"""UPDATE usuarios
                     SET estado = 'P'
-                    WHERE (noDocumento = 12345) and (estado = 'A');"""
+                    WHERE noDocumento = {noDocumento};"""
         try:
             self.cursor.execute(query)
             self.connection.commit()
-
         except Exception as e:
+            print(f"Error: {e}")
+            self.connection.rollback()
             raise
 
     def close(self):
         self.connection.close()
 
+response = requests.get("URL_DE_TU_API")
+json_data = response.json()
 
 database = DATABASE()
-
-database.eliminar_usuarios()
-
+for usuario in json_data['usuarios']:
+    database.eliminar_usuarios(usuario['noDocumento'])
 database.close()
